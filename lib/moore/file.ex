1e62545d66fa5baa2c7c3655f0179247f077fe04
Ernesto Malave <lex_nulla@protonmail.com>
2022-01-09 16:38:40 -0500

    parse file

--- lib/moore/file.ex
+++ lib/moore/file.ex
@@ -2,8 +2,11 @@ defmodule Moore.File do
   use Agent
 
   def start_link(path) do
-    with {:ok, bin} <- File.read(path) do
-      Agent.start_link(fn -> bin end)
+    with stream <- File.stream!(path, [], :line),
+         lines <- Enum.map(stream, &String.replace(&1, ~r/([ ]+|\n)/, " ")),
+         [headers, _empty | data] <- Enum.map(lines, &String.trim/1),
+         [headers | data] <- Enum.map([headers | data], &String.split(&1, " ")) do
+      Agent.start_link(fn -> {headers, data} end)
     end
   end
 end
--- test/file_test.exs
+++ test/file_test.exs
@@ -1,13 +1,24 @@
 defmodule Moore.FileTest do
   use ExUnit.Case
 
+  @headers ~w(Dy MxT MnT AvT HDDay AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP)
+
   describe "start_link/1" do
-    test "works when a file is present" do
-      assert {:ok, _} = Moore.File.start_link("./priv/weather.dat")
+    setup do
+      [path: "./priv/weather.dat"]
     end
 
-    test "fails when a file is not present" do
-      assert {:error, _} = Moore.File.start_link("./priv/im_not_real.dat")
+    test "works when a file is present", %{path: path} do
+      assert {:ok, _} = Moore.File.start_link(path)
+    end
+
+    test "raises when a file is not present" do
+      assert_raise File.Error, fn -> Moore.File.start_link("./priv/im_not_real.dat") end
+    end
+
+    test "splits the file", %{path: path} do
+      {:ok, file} = Moore.File.start_link(path)
+      assert {@headers, data} = :sys.get_state(file)
     end
   end
 end
