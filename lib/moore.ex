88a01416f634bea9bea8ef99fa9b3428daec7eb2
Ernesto Malave <lex_nulla@protonmail.com>
2022-01-09 19:33:39 -0500

    test football main function

--- lib/moore.ex
+++ lib/moore.ex
@@ -5,4 +5,9 @@ defmodule Moore do
     {:ok, file} = Moore.Weather.start_link(path)
     Logger.info("Day with smallest spread is: #{Moore.Weather.result(file)}")
   end
+
+  def team_with_the_smallest_goal_difference(path) do
+    {:ok, file} = Moore.Football.start_link(path)
+    Logger.info("Team with smallest goal difference is: #{Moore.Football.result(file)}")
+  end
 end
--- test/moore_test.exs
+++ test/moore_test.exs
@@ -8,4 +8,11 @@ defmodule MooreTest do
     assert capture_log(fn -> Moore.day_with_smallest_spread(path) end) =~
              "Day with smallest spread is: 14"
   end
+
+  test "team_with_the_smallest_goal_difference/1" do
+    path = "./priv/football.dat"
+
+    assert capture_log(fn -> Moore.team_with_the_smallest_goal_difference(path) end) =~
+             "Team with smallest goal difference is: Aston_Villa"
+  end
 end
